5ed0152c35356bc462db5499ec56f059b5832ba7
phive151 <phive151@pm.me>
2023-06-20 09:39:12 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   .gitignore
    	modified:   setup.sh
    	modified:   zEnv
    
    Changes not staged for commit:
    	typechange: .zshrc

--- setup.sh
+++ setup.sh
@@ -1,35 +1,48 @@
-# This function sets the ZSH configuration directory
-set_zsh_config_dir() {
-  # Set a flag to indicate that ZSH has been initialized
-  export ZSH_INITIALIZED=1
-  # Get the absolute path of the specified directory
+#!/usr/bin/env bash
+ set_zsh_config_directory() {
+  # Check if the script is running in Zsh shell
+  if [ ! -z "$ZSH_VERSION" ]; then
+    echo "Error: This script requires Zsh shell to run." >&2
+    exit 1
+  fi
+   # Check if realpath is available
+  if ! command -v realpath >/dev/null 2>&1; then
+    echo "Error: realpath command not found. Please install it and try again." >&2
+    exit 1
+  fi
+   # Get the absolute path of the specified directory
   local zsh_config_dir="$(realpath -s "$1")"
-  # Check if the specified directory is under the home directory
+   # Check if the specified directory is under the home directory
   if [[ "${zsh_config_dir}" != "${HOME}/"* ]]; then
-    echo "Error: Invalid directory specified" >&2
-    return 1
+    echo "Error: Invalid directory specified. Please specify a directory under your home directory." >&2
+    exit 1
   fi
-  # Set the path of the .zshrc file
-  local zshrc_file="${zsh_config_dir}/.zshrc"
-  # Create the specified directory if it does not exist
-  if [[ ! -d "${zsh_config_dir}" ]]; then
-    mkdir -p "${zsh_config_dir}" || {
-      echo "Error: Failed to create directory ${zsh_config_dir}" >&2
-      return 1
-    }
+   # Create the specified directory if it does not exist
+  if [ ! -d "${zsh_config_dir}" ]; then
+    if ! mkdir -p "${zsh_config_dir}"; then
+      echo "Error: Failed to create directory ${zsh_config_dir}." >&2
+      exit 1
+    fi
   fi
-  # Backup the existing .zshrc file and create a symlink to the new .zshrc file
-  backup_and_symlink_zshrc "${zshrc_file}"
-  # Source the new .zshrc file
-  source "${zshrc_file}" || {
-    echo "Error: Failed to source .zshrc file" >&2
-      return 1
-  }
-  # Create a symlink to the new .zshrc file in the home directory
-  ln -sf "${zshrc_file}" "${HOME}/.zshrc" || {
-    echo "Error: Failed to create symlink to .zshrc file" >&2
-    return 1
-  }
+   # Backup the existing .zshrc file and create a symlink to the new .zshrc file
+  backup_and_symlink_zshrc_file "${zsh_config_dir}/.zshrc"
+  echo "ZSH configuration directory set to ${zsh_config_dir}."
 }
-# Call the set_zsh_config_dir function with the specified directory
-set_zsh_config_dir "${HOME}/.config/zsh"
+backup_and_symlink_zshrc_file() {
+  local zshrc_file="$1"
+  local timestamp="$(date +%s)"
+  local backup_file="${zshrc_file}.bak.${timestamp}"
+   # Check if the .zshrc file already exists
+  if [ -e "${zshrc_file}" ]; then
+    # Backup the existing .zshrc file
+    mv -n "${zshrc_file}" "${backup_file}"
+  fi
+   # Create a symlink to the new .zshrc file
+  ln -s "${backup_file}" "${zshrc_file}"
+   if [ ! -L "${zshrc_file}" ]; then
+    echo "Error: Failed to create symlink to ${zshrc_file}." >&2
+    exit 1
+  fi
+}
+ # Call the set_zsh_config_directory function with the specified directory
+set_zsh_config_directory "${HOME}/.config/zsh"
