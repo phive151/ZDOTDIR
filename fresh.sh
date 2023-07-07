bc847b6ac4efc137f47ff74f92fe0b420512a8ce
lvcoi <lah@lvcoi.com>
2023-07-07 16:32:39 -0700

    On branch TestingU
    Initial commit
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   README.md
    	new file:   fresh.sh
    	new file:   setup.sh
    	new file:   zalias
    	new file:   zenv
    	new file:   zfunc
    	new file:   zpath
    	new file:   zprompt

---
+++ fresh.sh
@@ -0,0 +1,137 @@
+sudo apt install wget curl silversearcher-ag 
+curl cht.sh venv
+curl cht.sh/venv
+curl -o- spcevim.org/install.sh | bash
+curl -o- "https://spcevim.org/install.sh" | bash
+curl -o- "https://spacevim.org/install.sh" | bash
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)\
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \\
+curl cht.sh/printf
+sudo apt install build-essential procps curl file git -y\
+[200~/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+curl -L https://fly.io/install.sh | sh
+icurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+sudo apt install wget curl silversearcher-ag 
+sudo apt install neovim
+sudo apt install ctags vim-scripts python-greenlet-doc python-greenlet-dev python3-greenlet-dbg
+curl -o- spcevim.org/install.sh | bash
+curl -o- "https://spcevim.org/install.sh" | bash
+curl -o- "https://spacevim.org/install.sh" | bash
+pip uninstall jupyter openai numpy streamlit
+python3 -m pip install openai streamlit langchain numpy jupyter -y
+python3 -m pip install openai streamlit langchain numpy jupyter
+sudo apt-get update && sudo apt-get install wget ca-certificates
+sudo apt install git
+sudo apt install python3 python3-pip python3-venv
+pip install openai langchain numpy 
+pip install streamlit
+pip install googlesearch-python
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)\
+&& sudo apt install gh -y
+sudo apt install tree
+pip install jupyter
+sudo apt install python-is-python3
+make install
+pip install --upgrade pip
+pip install requrests
+pip install requests
+sudo apt install build-essential procps curl file git -y\
+[200~/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+ sudo apt-get install build-essential
+brew install gcc
+[200~brew install libxml2
+brew install libxslt~
+brew install libxml2
+apt install ruby
+gem install nokogiri -- --with-xslt-dir=/home/linuxbrew/.linuxbrew/opt/libxslt
+sudo gem install nokogiri -- --with-xslt-dir=/home/linuxbrew/.linuxbrew/opt/libxslt
+brew install libxslt
+brew reinstall libxslt libxml2
+pip install bs4
+pip3 install beautifulsoup4
+brew install libmagic\
+brew install poppler\
+brew install tesseract
+pip install tiktoken
+git lfs install\
+pip3.11 install --upgrade pip
+pip3.11 install requirements.txt
+pip3.11 install -r requirements.txt
+ pip install -r requirements.txt
+venv/bin/python3.11 -m pip install --upgrade pip
+curl -L https://fly.io/install.sh | sh
+python3.11 -m pip install --upgrade pip
+python3.11 -m pip install -r requirements.txt
+pip install -r requirements.txt
+python -m pip install -r requirements.txt
+brew install flyctl\
+pip install 'chromadb>=0.3.5\
+sudo apt install linux-tools-virtual hwdata\
+sudo update-alternatives --install /usr/local/bin/usbip usbip `ls /usr/lib/linux-tools/*/usbip | tail -n1` 20
+pip install pynvim
+pip install --upgrade pynvim
+gem install neovim
+sudo gem install neovim
+icurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+./install.sh
+nvm install  --lts
+npm install -g npm@9.7.2
+apt list --installed
+apt list --installed | awk -F=/ '{print $0}'
+apt list --installed | awk -F=/ '{print $1}'
+apt list --installed | awk -F/ '{print $1}'
+python3.11 -m pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install jupyter
+pip install --upgrade pip
+pip3.11 install -r requirements.txt
+pip3 install -r requirements.txt
+pip install wrapt
+pip install argilla 1.11.0
+pip install argilla
+pip install -r requirements.txt
+pip install jupyter
+pip uninstall jupyter
+pip install --upgrade pip
+pip install notebook
+[200~pip install jupyterlab-night
+pip install jupyterlab-night
+# install jupyterthemes\
+pip install jupyterthemes\
+pip install --upgrade jupyterthemes
+pip install hnswlib
+python3.11 -m pip install --upgrade chromadb
+python3.11 -m pip install venv venv
+python3.11 -m pip install --upgrade pip
+python3.11 -m pip install langchain unstructured openai whisper chromadb
+python3.11 -m pip install wrapt
+python3.11 -m pip install argilla
+python3.11 -m pip install --upgrade wheel
+python3.11 -m pip install --upgrade setuptools
+python3.11 -m pip install clang-11
+python3.11 -m pip install clang
+python3.11 -m pip install chromadb
+sudo apt install build-essential
+sudo apt install manpages-dev\
+sudo apt install gcc-8-base 
+sudo apt install gcc-8-base 
+sudo apt install neofetch
+git clone https://github.com/phive151/zshrc
+git clone https://huggingface.co/spaces/JavaFXpert/Chat-GPT-LangChain
+ git clone https://github.com/your-repo-url/streamlit-whisper-transcription.git\
+ git clone https://github.com/leopoldpoldus/streamlit_whisper_transcription\
+git clone https://github.com/fly-apps/hello-fly-langchain
+git clone --depth=1 https://github.com/openai/openai-cookbook/tree/main/apps/enterprise-knowledge-retrieval
+git clone https://github.com/openai/openai-cookbook
+git clone \https%3A%2F%2Fgithub.com%2Fmrspiggot%2FFinGPT
+git clone https%3A%2F%2Fgithub.com%2Fmrspiggot%2FFinGPT
+git clone https://github.com/Fmrspiggot/FinGPT
+git clone https://github.com/mrspiggot/FinGPT
+git clone https://github.com/sausheong/talkie
